c36d92390ebbff639805f27d35e3290ccc5d860d
francofallatti <francofallatti@gmail.com>
2024-04-17 19:29:15 -0300

    add useState

--- 01-counter-app/src/main.jsx
+++ 01-counter-app/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {CounterApp} from './CounterApp'
+import './styles.css'
 
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={2}/>
+        <CounterApp value={0}/>
     </React.StrictMode>
 )
