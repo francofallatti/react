1fc7c08586aa40cd914c158acbd7899e6342fecb
francofallatti <francofallatti@gmail.com>
2024-04-17 09:04:25 -0300

    arry map

--- 00 - introduccion/my-app/src/index.js
+++ 00 - introduccion/my-app/src/index.js
@@ -9,4 +9,25 @@ const persona = {
   },
 };
 
+// Crear clones de objetos
+const persona2 = { ...persona };
+persona2.apellido = "Messi";
+
 console.log({ persona });
+console.log({ persona2 });
+
+// Arrays
+
+const arry = [1, 2, 3, 4];
+const arry2 = [...arry, 5];
+
+const arry3 = arry2.map(function (number) {
+  return number * 2;
+});
+
+// Ppush modifica el objeto
+//arry.push(1);
+
+console.log(arry);
+console.log(arry2);
+console.log(arry3);
