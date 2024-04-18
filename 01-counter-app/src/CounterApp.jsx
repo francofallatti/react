093dceba4878a7db306f794a26b183edfca832b4
francofallatti <francofallatti@gmail.com>
2024-04-18 12:05:20 -0300

    add jest config

--- 01-counter-app/src/CounterApp.jsx
+++ 01-counter-app/src/CounterApp.jsx
@@ -8,12 +8,18 @@ export const CounterApp = ({value})=> {
     const handleAdd = () => {
         setCounter(counter + 1);
     }
+    const handleSub = () => {
+        setCounter(counter - 1);
+    }
+    const handleReset = () => {
+        setCounter(value);
+    }
     return <>
         <h1>CounterApp</h1>
         <h2> { counter } </h2>
-        <button onClick={handleAdd}>
-            +1
-        </button>
+        <button onClick={handleAdd}>+1</button>
+        <button onClick={handleSub}>-1</button>
+        <button onClick={handleReset}>Reset</button>
     </>;
 }
 
---
+++ 01-counter-app/test/demo.test.js
@@ -0,0 +1,8 @@
+describe("Pruebas en <DemoComponent/>", () => {
+  test("Esta prueba no debe fallar", () => {});
+
+  test("adds 1 + 2 to equal 3", () => {
+    const sum = 3;
+    expect(sum).toBe(3);
+  });
+});
