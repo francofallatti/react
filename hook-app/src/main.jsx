07d19853cc4d30300af95824b6420dc914baa1fa
francofallatti <francofallatti@gmail.com>
2024-04-21 20:04:39 -0300

    useMemo2

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -8,10 +8,11 @@ import "./index.css";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
-import { Memorize } from "./05-memos/Memorize";
+// import { Memorize } from "./05-memos/Memorize";
+import { MemoHook } from "./05-memos/MemoHook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   /* <React.StrictMode> */
-  <Memorize />
+  <MemoHook />
   /* </React.StrictMode> */
 );
