08755338b1f6ea7a8922215d566eaf713547d447
francofallatti <francofallatti@gmail.com>
2024-04-22 17:57:22 -0300

    heros SPA config

---
+++ heros-spa/src/HerosApp.jsx
@@ -0,0 +1,3 @@
+export const HerosApp = () => {
+  return <div>HerosApp</div>;
+};
---
+++ heros-spa/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import { HerosApp } from "./HerosApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HerosApp />
+  </React.StrictMode>
+);
