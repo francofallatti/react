d49196610949f7465faedc45adbc9302a91f4c33
francofallatti <francofallatti@gmail.com>
2024-04-24 10:48:18 -0300

    add login page

--- 05-journal-app/src/JournalApp.jsx
+++ 05-journal-app/src/JournalApp.jsx
@@ -1,3 +1,10 @@
+import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme";
+
 export const JournalApp = () => {
-  return <div>JournalApp</div>;
+  return (
+    <AppTheme>
+      <AppRouter />
+    </AppTheme>
+  );
 };
